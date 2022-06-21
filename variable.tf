fcac5254f9c4ec6c6929d46f8588e936abb9271b
Yaminiooty <107575274+Yaminiooty@users.noreply.github.com>
2022-06-21 23:51:09 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,8 +1,8 @@
 variable "access_key" {
-  default = "AKIA3XP4ZNK6CIEOOGV6"
+  default = "AKIA3XP4ZNK6E55OYTN3"
   }
 variable "secret_key" {
-  default = "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"
+  default = "RFoA5DLjY9RT+c9ruw5IfTf+biipFotfrH7sh7ji"
   }
 variable "region" {
   default = "ap-south-1"
